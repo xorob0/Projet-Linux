77ff248d188cfa2c4a552042bfed41251ed26288
Timothee Simon <xorob0@posteo.net>
2018-02-20 14:53:14 +0100

    Ajout de modularité

--- Scripts/networkConfig.sh
+++ Scripts/networkConfig.sh
@@ -4,14 +4,22 @@
 #ATTENTION, se mettre en NAT et executer en admin
 #ATTENTION, le nom de l'interface peut changer en fontion de la vm
 
+read -p "Entrez l'adresse IP" IP
+read -p "Entrez le masque de sous réseau" MASK
+read -p "Entrez votre gateway" GATEWAY
+read -p "Entrez votre DNS (Entrer pour 8.8.8.8)" DNS
+
+echo "Voici la liste de vos cartes réseau :"
+read -p "Entrez votre carte réseau" CARD
+
 echo "TYPE=Ethernet
 BOOTPROTO=static
-IPADDR=192.168.65.128
-NETMASK=255.255.255.0
-GATEWAY=192.168.65.2
-DNS1=192.168.65.2
-NAME=ens33
-ONBOOT=yes" > /etc/sysconfig/network-scripts/ifcfg-ens33
+IPADDR=${IP}
+NETMASK=${MASK}
+GATEWAY=${GATEWAY}
+DNS1=${DNS}
+NAME=${CARD}
+ONBOOT=yes" > /etc/sysconfig/network-scripts/ifcfg-${CARD}
 
 systemctl restart network.service
 
