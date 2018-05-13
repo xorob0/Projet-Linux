c59229190d5a643cbaa30a81500b4b5697e99514
Timothee Simon <xorob0@posteo.net>
2018-05-13 10:53:11 +0200

    Correction des espaces

--- Scripts/networkConfig.sh
+++ Scripts/networkConfig.sh
@@ -4,11 +4,11 @@ source Common.sh
 
 RootCheck
 
-IP = Argument $1 "192.168.1.2"
-MASK = Argument $2 "255.255.255.0"
-GATEWAY = Argument $3 "192.168.1.1"
-DNS = Arguemnt $4 "8.8.8.8"
-CARD = Argument $5 `ip link show | grep "2: " | cut -d " " -f2 | sed "s/://"`
+IP=Argument $1 "192.168.1.2"
+MASK=Argument $2 "255.255.255.0"
+GATEWAY=Argument $3 "192.168.1.1"
+DNS=Arguemnt $4 "8.8.8.8"
+CARD=Argument $5 `ip link show | grep "2: " | cut -d " " -f2 | sed "s/://"`
 
 
 echo "TYPE=Ethernet
