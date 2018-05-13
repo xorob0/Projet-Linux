3b373a198d1c1fa000add3c4a5197686410cd3cc
Timothee Simon <xorob0@posteo.net>
2018-05-13 10:56:29 +0200

    Amélioration du script

--- clientConfiguration/SSH_Linux.sh
+++ clientConfiguration/SSH_Linux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 source Common.sh
 
-USER = Argument $1 "root"
-IP = Argument $2 "192.168.1.2"
+UTILISATEUR=$(Argument $1 "root")
+IP=$(Argument $2 "192.168.1.2")
 
 # Installation ssh
-Installe openssh
+Installe openss
 
 #On génère une clé RSA et on stocke la clé privé dans /root/.ssh/id_rsa, ensuite on défini le mdp (passphrase = Test123*).
 #Cette procédure va permettre d'établir une connexion SSH par clé asymétrique. La clef publique est stockée dans un fichier qui porte le même nom plus une extension .pub
@@ -16,7 +16,7 @@ fi
 
 #On copie la clé public du client du dossier /root/.ssh/id_rsa.pub dans celle de l'utilisateur du serveur pour pouvoir s'y connecter sans mdp mais avec clé rsa
 
-ssh-copy-id -i /root/.ssh/id_rsa.pub $USER@$IP
+ssh-copy-id -i /root/.ssh/id_rsa.pub $UTILISATEUR@$IP
 
 #On se connecte 
-ssh $USER@$IP
+ssh $UTILISATEUR@$IP
