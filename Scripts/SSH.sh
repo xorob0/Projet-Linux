bb0c0abddc83a7d98fead3094607c61f4668e241
Timothee Simon <xorob0@posteo.net>
2018-05-13 11:50:38 +0200

    Utilisation de la fonction d'installation

--- Scripts/SSH.sh
+++ Scripts/SSH.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Installation du paquet open-ssh (vérification) permettant de partager un service ssh, par défaut il est normalement déjà installé
-yum install -y openssh-server
+Installe openssh-server
 
 # Utilsation du fichier de config
 cp ../Files/sshd_config /etc/ssh/ssh_config
