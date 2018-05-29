029d1c9be3fa1f1dc34b5e8e8fbffbd773bb977c
Timothee Simon <xorob0@posteo.net>
2018-05-29 15:02:49 +0200

    Ajout d'argument et de la configuration du fichier export

--- scripts/nfs/NFS.sh
+++ scripts/nfs/NFS.sh
@@ -5,6 +5,8 @@ RootCheck
 
 # Défault du dossier de partage
 DossierPartage = Argument $1 "/Partage"
+ARG = Argument $2 "rw,sync,no_root_squash,no_subtree_check)"
+IP = Argument $3 `ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 #création du dossier partagé si celui-ci n'existe pas encore
 if [ ! -d $DossierPartage ]
@@ -29,6 +31,7 @@ systemctl start nfs.service
 
 
 #Configuration du fichier /etc/exports
+echo "$DossierPartage	$IP$ARG" >> /etc/exports
 
 #On exporte le partage
 exportfs -a
