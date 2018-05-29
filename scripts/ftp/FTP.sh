1aafe40ee74be138c444a85cd26c4bf04418b27d
Timothee Simon <xorob0@posteo.net>
2018-05-29 16:19:56 +0200

    Utilisation du fichier common.sh

--- scripts/ftp/FTP.sh
+++ scripts/ftp/FTP.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 source ../Common.sh
-Installe vftp
+RootCheck
+
+Installe vsftp
+
+DossierPartage = Argument $1 "/Partage"
+
+systemctl start vsftp.service
+systemctl enable vsftp.service
+
+mkdir -p $DossierPartage
+
+
 echo "# Allow all connections \nvsftpd: ALL\n# IP adress range\nvsftpd: 10.0.0.0/255.255.255.0" > /etc/hosts.allow
 
