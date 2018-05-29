74459a5f4220edf243ae3e99b316d1e97d7a2faa
Timothee Simon <xorob0@posteo.net>
2018-05-29 15:12:25 +0200

    Meilleur configuration du script en utilisant le fichier common.sh

--- scripts/ntp/NTP.sh
+++ scripts/ntp/NTP.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+source ../Common.sh
+
+RootCheck
+
 #Installation de NTP
-sudo yum install ntp ntpdate
+Installe ntp ntpdate ntp-doc
 
 #On stop le service du serveur de temps chronyd et on le désactive ou sup$
-sudo systemctl stop chronyd
-sudo systemctl disable chronyd
+systemctl stop chronyd
+systemctl disable chronyd
 
 #On stop le service ntpd
-sudo systemctl stop ntpd
+systemctl stop ntpd
 
 #On met le serveur à la bonne heure au préalable
-sudo ntpdate be.pool.ntp.org
+ntpdate be.pool.ntp.org
 
 #Configuration du serveur ntp
-sudo nano /etc/ntp.conf   
+cp ntp.conf /etc/ntp.conf   
 
 #Démarrage et activation du service
-sudo systemctl start ntpd 
-sudo systemctl enable ntpd 
+systemctl start ntpd 
+systemctl enable ntpd 
 
 #On  test si le serveur est bien configuré (il doit s'afficher)
 ntpq –p 
-
