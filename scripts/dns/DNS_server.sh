d902c1fd127fa181a07834fee2ebe3038a2c7ab9
Timothee Simon <xorob0@posteo.net>
2018-06-16 23:21:36 +0200

    Correction d'une erreur

--- scripts/dns/DNS_server.sh
+++ scripts/dns/DNS_server.sh
@@ -4,7 +4,7 @@ source ../Common.sh
 
 RootCheck
 
-DOMAIN=`Argument $1 "linux.lan"`
+DOMAIN=`Argument $1 "linux"`
 
 IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
