996409ba4d893fc6f4422ba2c10b0fb58d1c2ae2
Timothee Simon <xorob0@posteo.net>
2018-06-19 14:20:22 +0200

    Suppression d'une dépendance génante

--- scripts/users/Yubico.sh
+++ scripts/users/Yubico.sh
@@ -2,7 +2,7 @@
 
 source ../Common.sh
 # Activation de l'EPEL
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 
 # Installation du module PAM
 Installe pam_yubico
